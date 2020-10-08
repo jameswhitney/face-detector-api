1c16f39f2419d90c1a68091ac45395a3b3774fbb
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2020-10-08 11:25:59 -0400

    Creates skeleton endpoints for signin and register

--- server.js
+++ server.js
@@ -1,9 +1,57 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+// Hard coded object to use for testing
+// Will later be removed once db is created
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
 
 app.get("/", (req, res) => {
-  res.send("Root response is working");
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(3000, () => {
@@ -13,7 +61,7 @@ app.listen(3000, () => {
 /*
 API Endpoints
 /root --> res = Root response is working
-/signIn --> POST = success/fail
+/signin --> POST = success/fail
 /register --> POST = user object
 /profile/:userId --> GET = user
 /image --> PUT --> update user object to reflect changes in score
