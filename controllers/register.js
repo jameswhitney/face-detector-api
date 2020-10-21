d49f3c2c888f48bc0891a7cccc2751af7ee6612b
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2020-10-21 14:49:11 -0400

    Removes extra bracket

--- controllers/register.js
+++ controllers/register.js
@@ -10,33 +10,32 @@ const handleRegister = (db, bcrypt) => (req, res) => {
   // if (emailExists) {
   //   return res.status(400).json("Unable to register. Duplicate entry");
   // } else {
-    const hash = bcrypt.hashSync(password);
-    db.transaction((trx) => {
-      trx
-        .insert({
-          hash: hash,
-          email: email,
-        })
-        .into("login")
-        .returning("email")
-        .then((loginEmail) => {
-          return trx("users")
-            .returning("*")
-            .insert({
-              name: name,
-              email: loginEmail[0],
-              joined: new Date(),
-            })
-            .then((user) => {
-              res.json(user[0]);
-            });
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    }).catch(() =>
-      res.status(400).json("Error ocurred during registration process")
-    );
-  }
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(() =>
+    res.status(400).json("Error ocurred during registration process")
+  );
 };
 
 module.exports = {
